5b568dba898b9009d3eb930082666726d0ee7c76
Vulnorn <mixadrbk31@gmail.com>
2024-09-24 00:16:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace NewC_1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberGrandmothers;
+            int minutesForOneGrandmother = 10;
+            int numberGrandmothersPerHour = 6;
+            int numberHours;
+            int numberMinutes;
+            int timeWhenThereNoQueue = 7;
+
+            Console.Write($"Вам не пвезло и вы заболели и решили ранним утром придти в поликлинику что бы попасть без очереди, но встали слишком поздно и в {timeWhenThereNoQueue} утра уже образовалась очередь. Сколько бабушек вы видите перед собой: ");
+            numberGrandmothers = Convert.ToInt32( Console.ReadLine() );
+
+            numberHours = numberGrandmothers / numberGrandmothersPerHour;
+            numberMinutes = numberGrandmothers * minutesForOneGrandmother - (numberHours * minutesForOneGrandmother);
+
+            Console.WriteLine($"Придя рано утром в полеклинику Вы должны отстоять {numberHours} часов и {numberMinutes} минут. Удачи.");
+        }
+    }
+}
